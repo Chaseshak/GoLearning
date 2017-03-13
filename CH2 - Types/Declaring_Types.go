c5ae54da5b7ecc6d8f7aba75a8438b315a88101c
Chase Schachenman <chaseshak@gmail.com>
2017-03-13 00:25:54 -0500

    Some string examples

--- CH2 - Types/Declaring_Types.go
+++ CH2 - Types/Declaring_Types.go
@@ -5,5 +5,15 @@ import(
 )
 
 func main() {
+	// Simple math
 	fmt.Println("1 + 1 = ", 1 + 1);
+
+	// Simple string
+	var hello string = "Hello, World!";
+
+	// Str length
+	fmt.Println("Length of hello var: ", len(hello));
+
+	// Gives byte representation since strings are characters which are bytes
+	fmt.Println("Char at index 3 (hello[3]): ", hello[3]);
 }
